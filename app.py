dfd61167d4bad8d5766039534f7a2a302f0af725
Luiz2002 <quineche44@gmail.com>
2025-01-30 22:16:43 -0500

    update config

---
+++ app.py
@@ -0,0 +1,19 @@
+# save this as app.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route('/send_data', methods=['POST'])
+def send_data():
+    # check if request has json data
+    if request.is_json:
+        data = request.get_json()
+        print(data)
+        return 'JSON received!'
+    else:
+        return 'Request is not JSON!'
+
